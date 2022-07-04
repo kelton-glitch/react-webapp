2e16dbf03849e874c6243ea94e2af7f7c1e594ba
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-07-04 17:49:32 +0100

    sign up

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -3,9 +3,54 @@ import React, {Component} from "react";
 export default class SignUp extends Component{
     render(){
         return(
-            <div>
-                <h3>SignUp Component</h3>
+           <form>
+            <h3>Sign Up</h3>
+            
+            <div className="mb-3">
+                <label>First Name</label>
+                <input 
+                type= "text"
+                className="form-control"
+                placeholder="First name"/>
             </div>
+
+            <div className="mb-3">
+                <label>Last Name</label>
+                <input 
+                type= "text"
+                className="form-control"
+                placeholder="Last name"/>
+            </div>
+
+            <div className="mb-3">
+                <label>Email address</label>
+                <input 
+                type= "email"
+                className="form-control"
+                placeholder="Enter email"/>
+            </div>
+
+            <div className="mb-3">
+                <label>Password</label>
+                <input 
+                type= "text"
+                className="form-control"
+                placeholder="Enter password"/>
+            </div>
+
+            <div className="d-grid">
+                <button type="submit" className="btn btn-primary">
+                    Sign Up
+                </button>
+            </div>
+            <p className="forgot-password text-right">
+                Already regstered? 
+            </p>
+
+            <p className="forgot-password text-right">
+                <a href="/sign-in">Sign In</a>
+            </p>
+           </form>
         );
     }
 }
