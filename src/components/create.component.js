39dd0aa6e77d135c928c16137984edfa6a32f192
kelton-glitch <kamgoelton89@gmail.com>
2022-07-13 22:37:40 +0100

    update

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -6,27 +6,27 @@ export default class CreateEmployee extends Component {
             <h3>Add New Employee</h3>
             <form>
                 <div className="mb-3">
-                    <label>Employee ID:  </label>
+                    <label>Employee ID:</label>
                     <input type ="text" class="form-control" placeholder="Enter your ID"/>
                 </div>
 
                 <div className="mb-3">
-                    <label>First Name:  </label>
+                    <label>First Name</label>
                     <input type ="text" class="form-control" placeholder="First Name" />
                 </div>
 
                 <div className="mb-3">
-                    <label>Last Name:  </label>
+                    <label>Last Name</label>
                     <input type ="text" class="form-control" placeholder="Last Name"/>
                 </div>
 
                 <div className="mb-3">
-                    <label>Email:  </label>
+                    <label>Email</label>
                     <input type ="email" class="form-control" placeholder="Enter email"/>
                 </div>
 
                 <div className="mb-3">
-                    <label>Position:  </label>
+                    <label>Position</label>
                     <input type ="text" class="form-control" placeholder="Enter position"/>
                 </div>
 
